fea143d6e8e424e2ed134dd0f7bcf4ac8196e31d
harry1453 <harryjamesphillips@gmail.com>
2018-05-16 21:52:44 +0100

    Slightly less yucky code

--- app/src/main/java/com/harrysoft/arduinocontrol/DeviceList.java
+++ app/src/main/java/com/harrysoft/arduinocontrol/DeviceList.java
@@ -3,7 +3,6 @@ package com.harrysoft.arduinocontrol;
 import android.bluetooth.BluetoothDevice;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 import java.util.Set;
@@ -17,12 +16,8 @@ import android.bluetooth.BluetoothAdapter;
 
 public class DeviceList extends AppCompatActivity {
 
-    private Button showPairedDevicesButton;
     private ListView deviceList;
-
     private BluetoothAdapter myBluetooth = null;
-    private Set<BluetoothDevice> pairedDevices;
-
     public static String EXTRA_ADDRESS = "device_address";
 
     @Override
@@ -30,73 +25,51 @@ public class DeviceList extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_device_list);
 
-        showPairedDevicesButton = (Button) findViewById(R.id.showPairedDevicesButton);
-        deviceList = (ListView) findViewById(R.id.pairedDevicesListView);
+        Button showPairedDevicesButton = findViewById(R.id.showPairedDevicesButton);
+        deviceList = findViewById(R.id.pairedDevicesListView);
 
         myBluetooth = BluetoothAdapter.getDefaultAdapter();
-        if(myBluetooth == null)
-        {
-            // Show a toast that the device has no bluetooth adapter
-            Toast.makeText(getApplicationContext(), "Bluetooth Device Not Available", Toast.LENGTH_LONG).show();
-            //Exit
-            finish();
-        }
-        else
-        {
-            if (myBluetooth.isEnabled())
-            { }
-            else
-            {
-                //Ask to the user turn the bluetooth on
+
+        if (myBluetooth != null) {
+            if (!myBluetooth.isEnabled()) {
                 Intent turnBTon = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
                 startActivityForResult(turnBTon,1);
             }
+        } else {
+            Toast.makeText(getApplicationContext(), "Bluetooth Not Available", Toast.LENGTH_LONG).show();
+            finish();
         }
 
-        showPairedDevicesButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v)
-            {
-                getPairedDevicesList(); //method that will be called
-            }
-        });
+        showPairedDevicesButton.setOnClickListener(v -> getPairedDevicesList());
     }
 
     private void getPairedDevicesList()
     {
-        pairedDevices = myBluetooth.getBondedDevices();
-        ArrayList list = new ArrayList();
+        Set<BluetoothDevice> pairedDevices = myBluetooth.getBondedDevices();
+        ArrayList<String> list = new ArrayList<>();
 
-        if (pairedDevices.size()>0)
-        {
-            for(BluetoothDevice bt : pairedDevices)
-            {
+        if (pairedDevices.size()>0) {
+            for(BluetoothDevice bt : pairedDevices) {
                 list.add(bt.getName() + "\n" + bt.getAddress()); //Get the device's name and the address
             }
-        }
-        else
-        {
+        } else {
             Toast.makeText(getApplicationContext(), "No Paired Bluetooth Devices Found.", Toast.LENGTH_LONG).show();
         }
 
-        final ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1, list);
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, list);
         deviceList.setAdapter(adapter);
-        deviceList.setOnItemClickListener(myListClickListener); //Method called when the device from the list is clicked
+        deviceList.setOnItemClickListener(myListClickListener);
 
     }
 
-    private AdapterView.OnItemClickListener myListClickListener = new AdapterView.OnItemClickListener()
-    {
-        public void onItemClick (AdapterView av, View v, int arg2, long arg3)
-        {
-            // Get the device MAC address, the last 17 chars in the View
-            String info = ((TextView) v).getText().toString();
-            String address = info.substring(info.length() - 17);
-            // Make an intent to start next activity.
-            Intent i = new Intent(DeviceList.this, RobotControl.class);
-            //Change the activity.
-            i.putExtra(EXTRA_ADDRESS, address); //this will be received at ledControl (class) Activity
-            startActivity(i);
-        }
+    private AdapterView.OnItemClickListener myListClickListener = (av, v, arg2, arg3) -> {
+        // THIS IS HORRIBLE!! THERE MUST BE A BETTER WAY TO DO THIS...
+
+        // Get the device MAC address, the last 17 chars in the View
+        String info = ((TextView) v).getText().toString();
+        String address = info.substring(info.length() - 17);
+        Intent i = new Intent(DeviceList.this, RobotControl.class);
+        i.putExtra(EXTRA_ADDRESS, address);
+        startActivity(i);
     };
 }
--- app/src/main/java/com/harrysoft/arduinocontrol/RobotControl.java
+++ app/src/main/java/com/harrysoft/arduinocontrol/RobotControl.java
@@ -5,9 +5,7 @@ import android.os.Bundle;
 import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
-import android.widget.SeekBar;
 import android.widget.Toast;
 import android.app.ProgressDialog;
 import android.bluetooth.BluetoothAdapter;
@@ -19,7 +17,6 @@ import java.util.UUID;
 public class RobotControl extends AppCompatActivity {
 
     Button upButton, downButton, leftButton, rightButton, stopButton, disconnectButton;
-    SeekBar brightness;
     String address = null;
     private ProgressDialog progress;
     BluetoothAdapter myBluetooth = null;
@@ -36,237 +33,121 @@ public class RobotControl extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_robot_control);
 
-        upButton = (Button)findViewById(R.id.upButton);
-        downButton = (Button)findViewById(R.id.downButton);
-        leftButton = (Button)findViewById(R.id.leftButton);
-        rightButton = (Button)findViewById(R.id.rightButton);
-        stopButton = (Button)findViewById(R.id.stopButton);
-        disconnectButton = (Button)findViewById(R.id.disconnectButton);
+        upButton = findViewById(R.id.upButton);
+        downButton = findViewById(R.id.downButton);
+        leftButton = findViewById(R.id.leftButton);
+        rightButton = findViewById(R.id.rightButton);
+        stopButton = findViewById(R.id.stopButton);
+        disconnectButton = findViewById(R.id.disconnectButton);
+
+        upButton.setOnClickListener(v -> sendUpCommand());
+        downButton.setOnClickListener(v -> sendDownCommand());
+        leftButton.setOnClickListener(v -> sendLeftCommand());
+        rightButton.setOnClickListener(v -> sendRightCommand());
+        stopButton.setOnClickListener(v -> sendStopCommand());
+        disconnectButton.setOnClickListener(v -> Disconnect());
 
         mConnectBT.execute();
-
-        //brightness = (SeekBar)findViewById(R.id.seekBar);
-
-        upButton.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                sendUpCommand();      //method to turn on
-            }
-        });
-
-        downButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v)
-            {
-                sendDownCommand();   //method to turn off
-            }
-        });
-
-        leftButton.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                sendLeftCommand();      //method to turn on
-            }
-        });
-
-        rightButton.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                sendRightCommand();      //method to turn on
-            }
-        });
-
-        stopButton.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                sendStopCommand();      //method to turn on
-            }
-        });
-
-        disconnectButton.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                Disconnect(); //close connection
-            }
-        });
-
-        /*brightness.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                if (fromUser==true)
-                {
-                    lumn.setText(String.valueOf(progress));
-                    try
-                    {
-                        btSocket.getOutputStream().write(String.valueOf(progress).getBytes());
-                    }
-                    catch (IOException e)
-                    {
-
-                    }
-                }
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
-        });*/
     }
 
-    private void msg(String s)
-    {
+    private void msg(String s) {
         Toast.makeText(getApplicationContext(),s,Toast.LENGTH_LONG).show();
     }
 
-    private void Disconnect()
-    {
-        if (btSocket!=null) //If the btSocket is busy
-        {
-            try
-            {
-                btSocket.close(); //close connection
+    private void Disconnect() {
+        if (btSocket != null) {
+            try {
+                btSocket.close();
+            } catch (IOException e) {
+                msg("Error");
             }
-            catch (IOException e)
-            { msg("Error");}
         }
-        finish(); //return to the first layout
+        finish();
     }
 
-    private void sendUpCommand()
-    {
-        if (btSocket!=null)
-        {
-            try
-            {
-                btSocket.getOutputStream().write("f".toString().getBytes());
-            }
-            catch (IOException e)
-            {
+    private void sendUpCommand() {
+        if (btSocket!=null) {
+            try {
+                btSocket.getOutputStream().write("f".getBytes());
+            } catch (IOException e) {
                 msg("Error");
             }
         }
     }
 
-    private void sendDownCommand()
-    {
-        if (btSocket!=null)
-        {
-            try
-            {
-                btSocket.getOutputStream().write("b".toString().getBytes());
-            }
-            catch (IOException e)
-            {
+    private void sendDownCommand() {
+        if (btSocket!=null) {
+            try {
+                btSocket.getOutputStream().write("b".getBytes());
+            } catch (IOException e) {
                 msg("Error");
             }
         }
     }
 
-    private void sendLeftCommand()
-    {
-        if (btSocket!=null)
-        {
-            try
-            {
-                btSocket.getOutputStream().write("l".toString().getBytes());
-            }
-            catch (IOException e)
-            {
+    private void sendLeftCommand() {
+        if (btSocket!=null) {
+            try {
+                btSocket.getOutputStream().write("l".getBytes());
+            } catch (IOException e) {
                 msg("Error");
             }
         }
     }
 
-    private void sendRightCommand()
-    {
-        if (btSocket!=null)
-        {
-            try
-            {
-                btSocket.getOutputStream().write("r".toString().getBytes());
-            }
-            catch (IOException e)
-            {
+    private void sendRightCommand() {
+        if (btSocket!=null) {
+            try {
+                btSocket.getOutputStream().write("r".getBytes());
+            } catch (IOException e) {
                 msg("Error");
             }
         }
     }
 
-    private void sendStopCommand()
-    {
-        if (btSocket!=null)
-        {
-            try
-            {
-                btSocket.getOutputStream().write("s".toString().getBytes());
-            }
-            catch (IOException e)
-            {
+    private void sendStopCommand() {
+        if (btSocket!=null) {
+            try {
+                btSocket.getOutputStream().write("s".getBytes());
+            } catch (IOException e) {
                 msg("Error");
             }
         }
     }
 
 
-    private class ConnectBT extends AsyncTask<Void, Void, Void>  // UI thread
-    {
-        private boolean ConnectSuccess = true; //if it's here, it's almost connected
+    private class ConnectBT extends AsyncTask<Void, Void, Void> {
+        private boolean ConnectSuccess = true;
 
         @Override
-        protected void onPreExecute()
-        {
-            progress = ProgressDialog.show(RobotControl.this, "Connecting...", "Please wait!!!");  //show a progress dialog
+        protected void onPreExecute() {
+            progress = ProgressDialog.show(RobotControl.this, "Connecting...", "Please wait!!!");
         }
 
         @Override
-        protected Void doInBackground(Void... devices) //while the progress dialog is shown, the connection is done in background
-        {
-            try
-            {
-                if (btSocket == null || !isBtConnected)
-                {
-                    myBluetooth = BluetoothAdapter.getDefaultAdapter();//get the mobile bluetooth device
+        protected Void doInBackground(Void... devices) {
+            try {
+                if (btSocket == null || !isBtConnected) {
+                    myBluetooth = BluetoothAdapter.getDefaultAdapter();
                     Log.e("gay", "address: " + address);
-                    BluetoothDevice dispositivo = myBluetooth.getRemoteDevice(address);//connects to the device's address and checks if it's available
-                    btSocket = dispositivo.createInsecureRfcommSocketToServiceRecord(myUUID);//create a RFCOMM (SPP) connection
+                    BluetoothDevice dispositivo = myBluetooth.getRemoteDevice(address);
+                    btSocket = dispositivo.createInsecureRfcommSocketToServiceRecord(myUUID);
                     BluetoothAdapter.getDefaultAdapter().cancelDiscovery();
-                    btSocket.connect();//start connection
+                    btSocket.connect();
                 }
-            }
-            catch (IOException e)
-            {
-                ConnectSuccess = false;//if the try failed, you can check the exception here
+            } catch (IOException e) {
+                ConnectSuccess = false;
             }
             return null;
         }
-        @Override
-        protected void onPostExecute(Void result) //after the doInBackground, it checks if everything went fine
-        {
-            super.onPostExecute(result);
 
-            if (!ConnectSuccess)
-            {
+        @Override
+        protected void onPostExecute(Void result) {
+            if (!ConnectSuccess) {
                 msg("Connection Failed. Is it a SPP Bluetooth? Try again.");
                 finish();
             }
-            else
-            {
+            else {
                 msg("Connected.");
                 isBtConnected = true;
             }
